e0dd1a3e8eb2ef46b0ee9bd388aab94bbd909cfd
Ben <benschel170@gmail.com>
2015-07-13 23:26:58 -0400

    Fixed snow

--- js/app.js
+++ js/app.js
@@ -1,73 +1,76 @@
 $(document).ready(function() {
-	var CodeFreeze = (function() {
-		var day;
-		var snowParticles 	= 40;
-		var openStatus 		= 'Open';
-		var closedStatus 	= 'Frozen';
-		var $alphaContainer = $('.alpha');
-		var $omegaContainer = $('.omega');
-		var $alphaStatus 	= $('.alpha .status');
-		var $omegaStatus 	= $('.omega .status');
+    var CodeFreeze = (function() {
+        var day;
+        var snowParticles   = 40;
+        var openStatus      = 'Open';
+        var closedStatus    = 'Frozen';
+        var $alphaContainer = $('.alpha');
+        var $omegaContainer = $('.omega');
+        var $alphaStatus    = $('.alpha .status');
+        var $omegaStatus    = $('.omega .status');
 
-		// Days that there is a code freeze
-		var rules = {
-			alpha : [1, 2],
-			omega : [3, 4]
-		};
+        // Days that there is a code freeze
+        var rules = {
+            alpha : [1, 2],
+            omega : [3, 4]
+        };
 
-		var app = {
-			init: function() {
-				var date = new Date();
-				day = date.getDay();
-				this.render();
-			},
+        var app = {
+            init: function() {
+                var date = new Date();
+                day = date.getDay();
+                this.render();
+            },
 
-			render: function() {
-				if (this._isAlphaClosed()) {
-					$alphaContainer.addClass('closed');
-					$alphaStatus.text(closedStatus);
+            render: function() {
+                if (this._isAlphaClosed()) {
+                    $alphaContainer.addClass('closed');
+                    $alphaStatus.text(closedStatus);
 
-					for (var i = 0; i < snowParticles; i++) {
-						$alphaContainer.find('.snow').append('<span></span>');
-					}
-				} else {
-					$alphaContainer.addClass('open')
-					$alphaStatus.text(openStatus);
-				}
+                    for (var i = 0; i < snowParticles; i++) {
+                        $alphaContainer.find('.snow').append('<span></span>');
+                    }
+                } else {
+                    $alphaContainer.addClass('open')
+                    $alphaStatus.text(openStatus);
+                }
 
-				if (this._isOmegaClosed()) {
-					$omegaContainer.addClass('closed');
-					$omegaStatus.text(closedStatus);
-					$omegaContainer.find('.snow').append('<span></span>');
-				} else {
-					$omegaContainer.addClass('open');
-					$omegaStatus.text(openStatus);
-				}
-			},
+                if (this._isOmegaClosed()) {
+                    $omegaContainer.addClass('closed');
+                    $omegaStatus.text(closedStatus);
 
-			_isAlphaClosed: function() {
-				for (var i = 0; i < rules.alpha.length; i++) {
-					if (day === rules.alpha[i]) {
-						return true;
-					}
-				}
+                    for (var i = 0; i < snowParticles; i++) {
+                        $omegaContainer.find('.snow').append('<span></span>');
+                    }
+                } else {
+                    $omegaContainer.addClass('open');
+                    $omegaStatus.text(openStatus);
+                }
+            },
 
-				return false;
-			},
+            _isAlphaClosed: function() {
+                for (var i = 0; i < rules.alpha.length; i++) {
+                    if (day === rules.alpha[i]) {
+                        return true;
+                    }
+                }
 
-			_isOmegaClosed: function() {
-				for (var i = 0; i < rules.omega.length; i++) {
-					if (day === rules.omega[i]) {
-						return true;
-					}
-				}
+                return false;
+            },
 
-				return false;
-			}
-		};
+            _isOmegaClosed: function() {
+                for (var i = 0; i < rules.omega.length; i++) {
+                    if (day === rules.omega[i]) {
+                        return true;
+                    }
+                }
 
-		return app;
-	})();
+                return false;
+            }
+        };
 
-	CodeFreeze.init();
+        return app;
+    })();
+
+    CodeFreeze.init();
 });
